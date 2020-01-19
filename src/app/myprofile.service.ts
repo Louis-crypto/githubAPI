53938978db687a5151e3dcc1e4763887046e881b
Louis-crypto <kinglouis745@gmail.com>
2020-01-19 09:32:56 +0300

    created a get profile data function

--- src/app/myprofile.service.ts
+++ src/app/myprofile.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyprofileService {
 
-  constructor() { }
+  private username: string;
+  private userid:'4f5f44dfdbc4ae999801';
+  private usersecret:'894ad411150950d99d47b1a0b35629c018cc4b43';
+
+  constructor(private http:HttpClient) { }
 }
